16c80707760866503cb48fdf3e6850db226affe8
comfyz <comfyz@qq.com>
2019-04-25 18:19:26 +0800

    乱七八糟胡乱看吧，慢慢丰富

--- src/main/java/xyz/comfyz/notes/Test.java
+++ src/main/java/xyz/comfyz/notes/Test.java
@@ -1,14 +1,36 @@
 package xyz.comfyz.notes;
 
+import com.sun.corba.se.spi.orbutil.threadpool.WorkQueue;
+import org.springframework.aop.*;
+import org.springframework.aop.framework.AopProxy;
+import org.springframework.aop.framework.DefaultAdvisorChainFactory;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.framework.ReflectiveMethodInvocation;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.HierarchicalBeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.sql.*;
+import java.util.concurrent.*;
 
 /**
  * @author :    comfy
@@ -22,7 +44,41 @@ public class Test {
     public static void main(String[] args) throws IllegalAccessException, SQLException {
         ApplicationContext context;
         BeanPostProcessor beanPostProcessor;
+        BeanFactory beanFactory;
+        HierarchicalBeanFactory hierarchicalBeanFactory;
+        ConfigurableBeanFactory configurableBeanFactory;
+        ApplicationContext context1;
+        Resource resource;
+        ResourceLoader resourceLoader;
+        BeanDefinition beanDefinition;
+        BeanDefinitionHolder beanDefinitionHolder;
+        BeanDefinitionRegistry beanDefinitionRegistry;
+        WebApplicationContext webApplicationContext;
+        ConfigurableApplicationContext configurableApplicationContext;
+        DefaultListableBeanFactory defaultListableBeanFactory;
+
+        BeforeAdvice beforeAdvice;
+        AfterAdvice afterAdvice;
+        AfterReturningAdvice afterReturningAdvice;
+        ThrowsAdvice throwsAdvice;
+        Pointcut pointcut;
+        JdkRegexpMethodPointcut jdkRegexpMethodPointcut;
+        Advisor advisor;
+        DefaultPointcutAdvisor defaultPointcutAdvisor;
+        Proxy proxy;
+        ProxyFactoryBean proxyFactoryBean;
+        AopProxy aopProxy;
+        ReflectiveMethodInvocation reflectiveMethodInvocation;
+        AopUtils aopUtils;
+        DefaultAdvisorChainFactory defaultAdvisorChainFactory;
         Enhancer enhancer = new Enhancer();
+        ForkJoinPool.commonPool();
+        ForkJoinPool forkJoinPool;
+        WorkQueue workQueue;
+        ForkJoinTask forkJoinTask;
+        RecursiveTask task;
+        ForkJoinWorkerThread forkJoinWorkerThread;
+        Executors.newFixedThreadPool()
 //        enhancer.setSuperclass();
         enhancer.setCallback(new MethodInterceptor() {
             @Override
